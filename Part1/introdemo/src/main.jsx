ed6285404abee1eccb2e43510d82e42581c49af5
Joehl1103 <73568630+Joehl1103@users.noreply.github.com>
2025-04-09 07:50:00 -0500

    "intro to react project"

---
+++ Part1/introdemo/src/main.jsx
@@ -0,0 +1,6 @@
+import ReactDOM from 'react-dom/client'
+
+import App from './App'
+
+// Renders the App component into the div element which is called 'root' in the html file
+ReactDOM.createRoot(document.getElementById('root')).render(<App/>)
