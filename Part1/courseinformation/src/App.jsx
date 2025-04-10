1d99e39722a90f24ae7fac27b9fc1ce38c80fbcb
Joehl1103 <73568630+Joehl1103@users.noreply.github.com>
2025-04-10 18:04:08 -0500

    "Submitting Exercises 1.1 & 1.2"

--- Part1/courseinformation/src/App.jsx
+++ Part1/courseinformation/src/App.jsx
@@ -1,18 +1,84 @@
-const App = (){
-  const course = "Half Stack application development"
+const CourseHeader = (props) => {
+  return (
+    <>
+      <h1>{props.courseName}</h1>
+    </>
+  )
+  
+}
+
+const Part1 = (props) => {
+  
+  return (
+    <>
+      <h2>{props.part1}</h2>
+      <p>Number of Exercises: {props.exercises1}</p>
+    </>
+
+  )
+}
+
+const Part2 = (props) => {
+  return (
+    <>
+      <h2>{props.part2}</h2>
+      <p>Number of Exercises: {props.exercises2}</p>
+    </>
+
+  )
+}
+
+const Part3 = (props) => {
+  return (
+    <>
+      <h2>{props.part3}</h2>
+      <p>Number of Exercises: {props.exercises3}</p>
+    </>
+
+  )
+}
+
+
+
+const Content = () => {
   const part1 = "Fundamentals of React"
   const exercises1 = 10
   const part2 = "Using props to pass data"
   const exercises2 = 7
   const part3 = "State of a component"
   const exercises3 = 14
+  
+  return (
+    <>
+      <Part1 part1={part1} exercises1={exercises1}/>
+      <Part2 part2={part2} exercises2={exercises2}/>
+      <Part3 part3={part3} exercises3={exercises3}/>
+    </>
+
+  )
+}
+
+const Total = (props) => {
+  return (
+    <p>Total number of exercises: {props.total} </p>
+  )
+}
+
+const App = ()=>{
+  const course = "Half-Stack Application Development"
+  const exercises1 = 10
+  const exercises2 = 7
+  const exercises3 = 14
+  const total = exercises1 + exercises2 + exercises3
 
   return (
     <>
-      <h1>{course}</h1>
-      <p>{part1} {exercises1}</p>
-      <p>{part2} {exercises2}</p>
-      <p>{part</p>
+      <CourseHeader courseName={course}/>
+      <Content/>
+      <hr/>
+      <Total total={total}/>
     </>
   )
 }
+
+export default App
