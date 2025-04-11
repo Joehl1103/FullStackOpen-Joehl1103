3533684e92e28ac16a41d427d24271a64d978d2d
Joehl1103 <73568630+Joehl1103@users.noreply.github.com>
2025-04-10 19:52:30 -0500

    "stopped through exercise 1.5"

--- Part1/courseinformation/src/App.jsx
+++ Part1/courseinformation/src/App.jsx
@@ -1,4 +1,28 @@
+// GLOBAL VARIABLES
+
+const course = "Half-Stack Application Development"
+const parts = [
+  {
+    name: "Fundamentals of React",
+    exercises: 10,
+  },
+  {
+    name: "Using props to pass data",
+    exercises: 7,
+  },
+  {
+    name: "State of a component",
+    exercises: 14,
+  }
+]
+
+const total = parts[0].exercises + parts[1].exercises + parts[2].exercises
+
+
+// COMPONENTS
+
 const CourseHeader = (props) => {
+  console.log(props)
   return (
     <>
       <h1>{props.courseName}</h1>
@@ -8,7 +32,7 @@ const CourseHeader = (props) => {
 }
 
 const Part1 = (props) => {
-  
+  console.log(props)
   return (
     <>
       <h2>{props.part1}</h2>
@@ -19,6 +43,7 @@ const Part1 = (props) => {
 }
 
 const Part2 = (props) => {
+  console.log(props)
   return (
     <>
       <h2>{props.part2}</h2>
@@ -29,6 +54,7 @@ const Part2 = (props) => {
 }
 
 const Part3 = (props) => {
+  console.log(props)
   return (
     <>
       <h2>{props.part3}</h2>
@@ -39,20 +65,13 @@ const Part3 = (props) => {
 }
 
 
-
 const Content = () => {
-  const part1 = "Fundamentals of React"
-  const exercises1 = 10
-  const part2 = "Using props to pass data"
-  const exercises2 = 7
-  const part3 = "State of a component"
-  const exercises3 = 14
   
   return (
     <>
-      <Part1 part1={part1} exercises1={exercises1}/>
-      <Part2 part2={part2} exercises2={exercises2}/>
-      <Part3 part3={part3} exercises3={exercises3}/>
+      <Part1 part1={parts[0].name} exercises1={parts[0].exercises}/>
+      <Part2 part2={parts[1].name} exercises2={parts[1].exercises}/>
+      <Part3 part3={parts[2].name} exercises3={parts[2].exercises}/>
     </>
 
   )
@@ -65,16 +84,15 @@ const Total = (props) => {
 }
 
 const App = ()=>{
-  const course = "Half-Stack Application Development"
-  const exercises1 = 10
-  const exercises2 = 7
-  const exercises3 = 14
-  const total = exercises1 + exercises2 + exercises3
-
+  
   return (
     <>
       <CourseHeader courseName={course}/>
-      <Content/>
+      <Content
+        part1={parts[0].name} exercises1={parts[0].exercises}
+        part2={parts[1].name} exercises2={parts[1].exercises}
+        part3={parts[2].name} exercises3={parts[2].exercises}
+      />
       <hr/>
       <Total total={total}/>
     </>
