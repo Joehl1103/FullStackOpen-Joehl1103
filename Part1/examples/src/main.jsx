331167e19dacb0c9ef78aa1845d3c767699d3ef3
Joehl1103 <73568630+Joehl1103@users.noreply.github.com>
2025-04-12 16:14:03 -0500

    "examples for component state"

---
+++ Part1/examples/src/main.jsx
@@ -0,0 +1,10 @@
+import ReactDOM from 'react-dom/client'
+
+import App from './App'
+console.log("App imported from App")
+
+ReactDOM.createRoot(document.getElementById('root')).render(<App/>)
+
+
+
+
