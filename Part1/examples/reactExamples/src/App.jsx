d1778090e2794ad4b992c2f9c6541dae0afee317
Joehl1103 <73568630+Joehl1103@users.noreply.github.com>
2025-04-17 13:59:53 -0500

    "asynchronous state updating"

--- Part1/examples/reactExamples/src/App.jsx
+++ Part1/examples/reactExamples/src/App.jsx
@@ -1,23 +1,41 @@
 import {useState} from 'react'
 
-const App = () => {
-  const [clicks, setClicks] = useState({
-      left:0,right:0
-    }
-  )
 
-  const handleLeftClick = () => setClicks({...clicks,left: clicks.left + 1})
-  
-  const handleRightClick = () => setClicks({...clicks,right: clicks.right + 1})
+const App = () => {
+  const [left, setLeft] = useState(0)
+  const [right, setRight] = useState(0)
+  const [allClicks,setAll] = useState([])
+  const [counter, setCounter] = useState(1)
+  const [total, setTotal] = useState(0)
+     
+  const handleLeftClick = () => {
+    setCounter(counter+1)
+    setAll(allClicks.concat(`${counter}: L`))
+    console.log('left before',left)
+    setLeft(left+1)
+    console.log('left after',left) 
+    setTotal(left+right)
+  }
+    
+  const handleRightClick = () => {
+    setCounter(counter+1)
+    setAll(allClicks.concat(`${counter}: R`))
+    console.log('right before',right)
+    setRight(right+1)
+    console.log('right after',right)
+    setTotal(left+right)
+  }
   
   return (
     <>
-     {clicks.left}
+     {left}
       <button onClick={handleLeftClick}>left</button>
       <button onClick={handleRightClick}>right</button>
-      {clicks.right}
+      {right}
+      <p>This is an array of all the clicks: [ {allClicks.join(', ')} ]</p>
+      <p>Total clicks: <b>{total}</b></p>
     </>
-  )
+    )
 }
 
 export default App
