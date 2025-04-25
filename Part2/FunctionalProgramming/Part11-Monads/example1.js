14422cdd2c6922ee4f933c56b7bc93f171408f1e
Joehl1103 <73568630+Joehl1103@users.noreply.github.com>
2025-04-25 17:30:36 -0500

    "monads-stuck on the method not returning anything"

--- Part2/FunctionalProgramming/Part11-Monads/example1.js
+++ Part2/FunctionalProgramming/Part11-Monads/example1.js
@@ -1,7 +1,8 @@
-const fetch = require('node-fetch')
+// const fetch = require('node-fetch')
 const Bacon = require('baconjs')
 
 function getInPortuguese(word){
+    console.log("Initializing functions")
     const apiKey = "AIzaSyCYeC-819nJFtzXguCplQ9_A2avbLuYRzA"
     const url = 'https://www.googleapis.com' + 
         '/language/translate/v2' +
@@ -9,18 +10,48 @@ function getInPortuguese(word){
         '&source=en' +
         '&target=pt' +
         '&q=' + encodeURIComponent(word)
-    const promis = fetch(url)
-        .then(response => response.json)
-        .then(parsedResponse => 
+    console.log("URL: ",url)
+    const promise = fetch(url)
+        .then(response => {
+            response.json
+            console.log("Json response: ",response.json())
+            })
+        .then(parsedResponse => {
+            pro = parsedResponse.data.translations[0].translatedText
+            if (pro.length === 0){
+                console.log("empty")
+                return
+            }
+            console.log(
             parsedResponse  
                 .data
-                .translations(0)
-                .translatedText
-        )
+                .translations[0]
+                .translatedText)
+            })
+        // .then(data => {
+        //     if(data === null || data === undefined){
+        //         console.log("promise is empty")
+        //     } else if (Array.isArray(data) && data.length === 0){
+        //         console.log('data is an empty array')
+        //     }
+        //     }
+        // )
+    // // console.log("Promise type",typeof promise)
+    // console.log("Promise: ",JSON.stringify(promise)) 
+    // if (promise.length === 0){
+    //     console.log("promise is empty")
+    //     return
+    // }
     const stream = Bacon.fromPromise(promise)
+    console.log("Returning stream")
     return stream
 
 }
 
-getInPortuguese('meal')
-    .onValue(word => console.log(word))
+// try {
+console.log("Getting in Portuguese")
+getInPortuguese('meal').onValue(word => console.log("Value: ",word))
+console.log("Got in Portuguese")
+// } catch (e){
+//     console.error(e.message)
+// }
