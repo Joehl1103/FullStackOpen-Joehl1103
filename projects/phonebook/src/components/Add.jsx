e6514dd50058ee7d8e4bae33653bf44600fc690b
Joehl1103 <73568630+Joehl1103@users.noreply.github.com>
2025-05-01 06:33:47 -0500

    "2.10 added state and event handler functions back to the main app"

--- projects/phonebook/src/components/Add.jsx
+++ projects/phonebook/src/components/Add.jsx
@@ -1,36 +1,4 @@
-import {useState} from 'react'
-
 const Add = (props) => {
-   
-      const [newName,setNewName] = useState('')
-      const [newPhone,setNewPhone] = useState('')
-
-    function addPersonToList(event){
-        // prevent default form behavior: re-rendering the page
-        event.preventDefault()
-    
-        const newNameExists = checkForExistingName()
-        console.log("Does newName exist? ",newNameExists)
-        if (newNameExists){
-          alert(`${newName} already exists in the phonebook`)
-          return
-        }
-        // create the new object using newName, set by the onChange event handler
-        const newObject = {name: newName,number: newPhone}
-        // destrcture persons in a new array, and include the newObject
-        const personsCopy = [...props.persons, newObject]
-        // set the copy to the original
-        props.setPersons(personsCopy)
-        // reset newName
-        setNewName('')
-      }
-
-      function checkForExistingName(){
-        console.log("checkForExistingName newName: ",newName)
-        const existingName = props.persons.filter(person => person.name.toLowerCase().includes(newName.toLowerCase))
-        console.log("existingName: ",typeof existingName, " ",JSON.stringify(existingName))
-        return existingName.length !== 0 ? true : false
-      }
 
 return (
     <div>
@@ -38,13 +6,13 @@ return (
         {/* add a custom submission function reference directly to the form so as not to trigger
         the page to rerender*/}
 
-        <form onSubmit={addPersonToList}>
+        <form onSubmit={props.addPersonToList}>
         <div>
             Name: <input
                     type="text"
-                    value={newName}
+                    value={props.newName}
                     onChange={e => {
-                    setNewName(e.target.value)
+                    props.setNewName(e.target.value)
 
                     }}/>
         </div>
@@ -53,8 +21,8 @@ return (
             <input
                 type="tel"
                 pattern="\(\d{3}\)\s\d{3}-\d{4}"
-                value={newPhone}
-            onChange={e => setNewPhone(e.target.value)}
+                value={props.newPhone}
+            onChange={e => props.setNewPhone(e.target.value)}
             placeholder="(000) 000-0000"
             />
 
