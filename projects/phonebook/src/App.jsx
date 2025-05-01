50e65b011faa401305c5138ad7a9d8ac66563eff
Joehl1103 <73568630+Joehl1103@users.noreply.github.com>
2025-05-01 06:33:47 -0500

    "2.10 added state and event handler functions back to the main app"

--- projects/phonebook/src/App.jsx
+++ projects/phonebook/src/App.jsx
@@ -21,14 +21,37 @@ function App() {
           number: '(123) 234-2342'
         }
  ])
+ const [newName,setNewName] = useState('')
+ const [newPhone,setNewPhone] = useState('')
  const [searchTerm,setSearchTerm] = useState('')
 
-// TODO add back event handlers to main component
-// TODO add back any state variables that are missing and pass them to the child components
- 
+ function addPersonToList(event){
+  // prevent default form behavior: re-rendering the page
+  event.preventDefault()
 
-  
-  // Why is the PersonDisplay not rendering
+  const newNameExists = checkForExistingName()
+  console.log("Does newName exist? ",newNameExists)
+  if (newNameExists){
+    alert(`${newName} already exists in the phonebook`)
+    return
+  }
+  // create the new object using newName, set by the onChange event handler
+  const newObject = {name: newName,number: newPhone}
+  // destrcture persons in a new array, and include the newObject
+  const personsCopy = [...persons, newObject]
+  // set the copy to the original
+  setPersons(personsCopy)
+  // reset newName
+  setNewName('')
+}
+
+function checkForExistingName(){
+  console.log("checkForExistingName newName: ",newName)
+  const existingName = persons.filter(person => person.name.toLowerCase().includes(newName.toLowerCase))
+  console.log("existingName: ",typeof existingName, " ",JSON.stringify(existingName))
+  return existingName.length !== 0 ? true : false
+}
+
   return (
     <>
       <h2>Phonebook</h2>
@@ -38,6 +61,11 @@ function App() {
       <Add
         persons={persons}
         setPersons={setPersons}
+        addPersonToList={addPersonToList}
+        newName={newName}
+        setNewName={setNewName}
+        newPhone={newPhone}
+        setNewPhone={setNewPhone}
         />
         <h3>List</h3>
       <PersonDisplay 
