49106e8d2709bfc8070e70ebe1858ddc3d176a83
Joehl1103 <73568630+Joehl1103@users.noreply.github.com>
2025-05-02 07:30:01 -0500

    "adding useEffect hook to fetch the data from the mock server and display in the notes state"

--- projects/notes/src/main.jsx
+++ projects/notes/src/main.jsx
@@ -1,22 +1,22 @@
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
-const notes = [
-  {
-    id:1,
-    content: 'HTML is easy',
-    important: Math.random() < 0.5
-  },
-  {
-    id:2,
-    content: 'CSS is a little less easy',
-    important:true 
-  },
-  {
-    id:3,
-    content: 'JS is not easy at all',
-    important: Math.random() < 0.5
-  }
-]
+// const notes = [
+//   {
+//     id:1,
+//     content: 'HTML is easy',
+//     important: Math.random() < 0.5
+//   },
+//   {
+//     id:2,
+//     content: 'CSS is a little less easy',
+//     important:true 
+//   },
+//   {
+//     id:3,
+//     content: 'JS is not easy at all',
+//     important: Math.random() < 0.5
+//   }
+// ]
 
-createRoot(document.getElementById('root')).render(<App notes={notes} />)
+createRoot(document.getElementById('root')).render(<App/>)
