fcb97cb7e3aa3d68a9b7db12714389792c637d5f
Joehl1103 <73568630+Joehl1103@users.noreply.github.com>
2025-05-16 15:41:01 -0500

    "finalized the country search program basic structure"

--- projects/data-for-countries/src/App.jsx
+++ projects/data-for-countries/src/App.jsx
@@ -3,17 +3,15 @@ import Matches from './Components/Matches'
 import searchService from './services/searchService'
 
 function App() {
-  const [matches,setMatches] = useState([])
+  const [matches,setMatches] = useState(null)
   const [searchTerm, setSearchTerm] = useState('')
  // const searchKeys = useState(['name','capital','area','languages','flag'])
 
   const search = (event) => {
-    console.log(event.target.value)
+    console.log("search term: ",event.target.value)
     setSearchTerm(event.target.value)
     const searchTermLowerCase = searchTerm.toLowerCase()
     console.log("Search term lower case:",searchTermLowerCase)
-    // TODO add getAll to get object of all items before searching through them
-    
     
     searchService
       .getAll()
@@ -28,51 +26,23 @@ function App() {
             if (key === "name"){
               if (value.common && value.common.toLowerCase().includes(searchTermLowerCase)){
                 names.push(value.common)
-              } else {
-                console.log("NO COMMON NAME")
-              }
+              } 
             }
-
           }
-
         }
+        console.log("Returning array of names,",names)
         return names
-        //  console.log(`Names: ${names}`)
       })
        
       .then(names => {
         if (names.length === 1){
-          console.log("Only 1 name",names)
           searchAndReturnOneCountryObject(names)
         }
         setMatches(names)
-        // console.log(`Names ${names}`)
       })
       .catch(error => {
         console.log(error.message)
       })
-
-   
- 
-    // search through the entire object of objects to find any names that match and return their official names
-    // const names = []
-
-
-
-    // for (const [key,value] of entries){
-    //  
-    //   }
-    // }
-
-    // setMatches(names)
-
-    // note that I think the solution should probably be different depending on whether or not you are searching for a specific subset of items vs just the names
-    // this should probably be a different kind of search function
-
-    // TODO first search through the entire object to find a matching name in lower case and add that entire object to a temporary array
-
-    // TODO second, search through the temporary array to find the specific properties and add those to the final array
-
 
   }
 
@@ -86,8 +56,6 @@ function App() {
               name: response.name.common,
               capital: response.capital[0],
               area: response.area,
-              // note that this is an object
-              // TODO adjust the Matches method
               languages: response.languages,
               flagURL: response.flags.png // are flags of different kinds or all they all emoji characters?
           }
@@ -95,8 +63,8 @@ function App() {
         console.log("Matches set to oneCountryObject", oneCountryObject)
   })
 }
-
-  
+// RENDER
+  if (matches === null){
   return (
     <>
       <label>find countries: </label>
@@ -105,11 +73,24 @@ function App() {
           type="text" 
           value={searchTerm} 
           onChange={search}/>
-
-      <Matches matches={matches}/>
-  
     </>
   )
+  } else {
+    return (
+      <>
+        <label>find countries: </label>
+    
+        <input 
+            type="text" 
+            value={searchTerm} 
+            onChange={search}/>
+
+        <Matches matches={matches}/>
+    
+      </>
+    )
+
+  }
 }
 
 export default App
--- projects/data-for-countries/src/Components/Matches.jsx
+++ projects/data-for-countries/src/Components/Matches.jsx
@@ -1,12 +1,15 @@
 const Matches = (matches) => {
 
+    console.log("Matches in Matches Component:",matches,"Type of Matches: ",typeof matches)
+
     /* note that Matches is either an array of names or an object depending on
     whether it is an array of just names or an object containing all of the country's info
     */
-    console.log("Matches:",matches,"Type of Matches: ",typeof matches)
-    let nMatchCount = null;
-    if (Array.isArray(matches)){
+    if (Array.isArray(matches.matches)){
+        let nMatchCount = null;
+
         const matchesArray = matches.matches
+
         function countNumberOfMatches(matchesArray){
             let count = 0;
             for (let i = 0; i < matchesArray.length;i++){
@@ -16,55 +19,66 @@ const Matches = (matches) => {
         }
 
         nMatchCount = countNumberOfMatches(matchesArray)
-         } else if (nMatchCount > 10){
-        return (
-            <div>
-                <p>Too many matches, please be more specific</p>
-            </div>
-        )
-        // >1 && < 11
-    } else {
-        const countryArray = matches.matches
-    
-        return (
-            <div>
-            {countryArray.map(name => {
-                return <p key={name}>{name}</p>
-            })}
-            </div>
-        )
-    }
-    }
         
-        // 1 match
-    if (nMatchCount === 1){
-        const countryObject = matches[0]
-        // TODO add a search method that is specific to searching for a single country
-        // TODO object and returns all the specific properties
-        const languagesArray = countryObject.languages
-        // console.log(languagesArray)
-        return (
-            <div>
-                <h1>{countryObject.name}</h1>
-                <p>
-                    Capital: {countryObject.capital}<br/>
-                    Area: {countryObject.area}
-                </p>
-                <h2>Languages</h2>
-                <ul>{languagesArray.map(language => {
-                        
-                            return <li key={language}>{language}</li>
-                        }
-                        )
-                    }
-                </ul>
-                <p>Flag:</p><br/>
-                <img src={countryObject.flagURL}/>
+        if (nMatchCount > 15){
+            return (
+                <div>
+                    <p>Too many matches, please be more specific</p>
+                </div>
+            )
+        // >1 && < 11
+        } else {
+            const countryArray = matches.matches
+        
+            return (
+                <div>
+                {countryArray.map(name => {
+                    return <p key={name}>{name}</p>
+                })}
+                </div>
+            )
+        }
+    } else {
+        const countryObject = matches.matches
+        const countryObjectLanguageObject = countryObject.languages
+        if (countryObjectLanguageObject != undefined){
+            const languageObjectEntries = Object.entries(countryObjectLanguageObject)
+             const languagesArray = []
+      
+            for (const [key,value] of languageObjectEntries){
+                languagesArray.push(value)
+            }
 
-            </div>
-        )
-        // > 10
-   
+            return (
+                <div>
+                    <h1>{countryObject.name}</h1>
+                    <p>
+                        Capital: {countryObject.capital}<br/>
+                        Area: {countryObject.area}
+                    </p>
+                    <h2>Languages</h2>
+                    <ul>{languagesArray.map(language => {
+                            
+                                return <li key={language}>{language}</li>
+                            }
+                            )
+                        }
+                    </ul>
+                    <p>Flag:</p><br/>
+                    <img src={countryObject.flagURL}/>
+
+                </div>
+            )
+
+        } else {
+            console.log("Country Object Language Object undefined")
+
+            return (
+                null
+            )
+        }
+       
+    }
 }
 
 export default Matches
