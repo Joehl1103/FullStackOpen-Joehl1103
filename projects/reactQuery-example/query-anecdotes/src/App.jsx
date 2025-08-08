e035024bb117b52ebb8ff8b46beb8f095b3997c4
Joehl1103 <73568630+Joehl1103@users.noreply.github.com>
2025-08-08 15:51:05 -0500

    implemented fetching and displaying data

---
+++ projects/reactQuery-example/query-anecdotes/src/App.jsx
@@ -0,0 +1,50 @@
+import AnecdoteForm from './components/AnecdoteForm'
+import Notification from './components/Notification'
+import { useQuery } from '@tanstack/react-query'
+import axios from 'axios'
+
+const App = () => {
+
+  const result = useQuery({
+    queryKey: ['notes'],
+    queryFn: () => axios.get('http://localhost:3001/anecdotes').then(res => res.data)
+  })
+
+  if (result.isPending) {
+    return <div>Data is loading...</div>
+  }
+  if (result.isError) {
+    return <div>anecdote service is not available due to problems in the server: {result.error.message}</div>
+  }
+
+  console.log(JSON.parse(JSON.stringify('result of query', result)))
+
+  const handleVote = (anecdote) => {
+    console.log('vote')
+  }
+
+  const anecdotes = result.data
+
+  return (
+    <div>
+      <h3>Anecdote app</h3>
+
+      <Notification />
+      <AnecdoteForm />
+
+      {anecdotes.map(anecdote =>
+        <div key={anecdote.id}>
+          <div>
+            {anecdote.content}
+          </div>
+          <div>
+            has {anecdote.votes}
+            <button onClick={() => handleVote(anecdote)}>vote</button>
+          </div>
+        </div>
+      )}
+    </div>
+  )
+}
+
+export default App
