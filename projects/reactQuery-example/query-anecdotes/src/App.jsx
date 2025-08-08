4ff003aed4e00408ba5b47ab681acc9a5dfd96c8
Joehl1103 <73568630+Joehl1103@users.noreply.github.com>
2025-08-08 16:57:03 -0500

    added vote incrementation

--- projects/reactQuery-example/query-anecdotes/src/App.jsx
+++ projects/reactQuery-example/query-anecdotes/src/App.jsx
@@ -1,11 +1,30 @@
 import AnecdoteForm from './components/AnecdoteForm'
 import Notification from './components/Notification'
-import { useQuery } from '@tanstack/react-query'
+import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query'
 import axios from 'axios'
 
 const App = () => {
 
+  const queryClient = useQueryClient()
+
+  const incrementVote = (changedAnecdote) => {
+    return axios.put(`${baseUrl}/${changedAnecdote.id}`, changedAnecdote).then(res => res.data)
+  }
+
+  const incrementVoteMutation = useMutation({
+    mutationFn: incrementVote,
+    onSuccess: (changedAnecdote) => {
+      const anecdotes = queryClient.getQueryData(['anecdotes'])
+      queryClient.setQueryData(['anecdotes'], anecdotes.map(a => {
+        if (a.id === changedAnecdote.id) {
+          return changedAnecdote
+        }
+        return a
+      }))
+    }
+  })
   const baseUrl = 'http://localhost:3001/anecdotes'
+
   const result = useQuery({
     queryKey: ['anecdotes'],
     queryFn: () => axios.get(baseUrl).then(res => res.data)
@@ -18,14 +37,14 @@ const App = () => {
     return <div>anecdote service is not available due to problems in the server: {result.error.message}</div>
   }
 
-  console.log(JSON.parse(JSON.stringify('result of query', result)))
+  const anecdotes = result.data
 
 
   const handleVote = (anecdote) => {
-    console.log('vote')
+    const changedAnecdote = { ...anecdote, votes: anecdote.votes + 1 }
+    incrementVoteMutation.mutate(changedAnecdote)
   }
 
-  const anecdotes = result.data
 
   return (
     <div>
--- projects/reactQuery-example/query-anecdotes/src/components/AnecdoteForm.jsx
+++ projects/reactQuery-example/query-anecdotes/src/components/AnecdoteForm.jsx
@@ -4,37 +4,33 @@ import axios from 'axios'
 const AnecdoteForm = () => {
   const queryClient = useQueryClient()
 
-  const baseUrl = 'http://localhost:3001/anecdotes'
-
   const createAnecdote = (newAnecdote) => {
-    console.log('anecdote in createAnecdote', newAnecdote)
     const result = axios.post(baseUrl, newAnecdote)
       .then(res => {
-        console.log('post request went through successfully', res)
         return res.data
       })
       .catch((err) => {
         console.log(`Error in post request ${err.message}`)
       })
-    console.log('result in createAnecdote', result)
     return result
   }
-
   const newAnecdoteMutation = useMutation({
     mutationFn: createAnecdote,
     onSuccess: (newAnecdote) => {
-      console.log('newAnecdote in newAnecdoteMutation', newAnecdote)
       const notes = queryClient.getQueryData(['anecdotes'])
       queryClient.setQueryData(['anecdotes'], notes.concat(newAnecdote))
     }
   })
+
+  const baseUrl = 'http://localhost:3001/anecdotes'
+
+
 
   const onCreate = (event) => {
     event.preventDefault()
     const content = event.target.anecdote.value
     newAnecdoteMutation.mutate({ content: content, votes: 0 })
   }
-
 
   return (
     <div>
