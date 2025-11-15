d81280bf0cf27a68d8291bc50dd46cbc2879d8d3
Joehl1103 <73568630+Joehl1103@users.noreply.github.com>
2025-11-14 21:00:08 -0600

    modified bmiCalc to take command line arguments and refactored and added tests

--- Part9/first-exercises/jest.config.cjs
+++ Part9/first-exercises/jest.config.cjs
@@ -1,11 +1,17 @@
-const { createDefaultPreset } = require("ts-jest");
-
-const tsJestTransformCfg = createDefaultPreset().transform;
-
 /** @type {import("jest").Config} **/
 module.exports = {
+  preset: "ts-jest/presets/default-esm",
   testEnvironment: "node",
+  extensionsToTreatAsEsm: [".ts"],
+  moduleNameMapper: {
+    "^(\\.{1,2}/.*)\\.ts$": "$1",
+  },
   transform: {
-    ...tsJestTransformCfg,
+    "^.+\\.ts$": [
+      "ts-jest",
+      {
+        useESM: true,
+      },
+    ],
   },
 };
